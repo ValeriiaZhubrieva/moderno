e753dcf0961478be0885b5d80554e63295f155f4
ValeriiaZhubrieva <perchik010102@gmail.com>
2020-07-17 21:36:20 +0300

    added about-page, 404 and product-one

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ gulp.task('script', function(){
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
         'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
@@ -38,7 +39,7 @@ gulp.task('style', function(){
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
         'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
-
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -51,7 +52,7 @@ gulp.task('html', function(){
 });
 
 gulp.task('js', function(){
-    return qulp.src('app/js/*.js')
+    return gulp.src('app/js/*.js')
             .pipe(browserSync.reload({stream:true}))
 });
 
